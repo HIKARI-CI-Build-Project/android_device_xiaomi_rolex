dc2c049318c2ec7336fb75c5dbefbcdd5360019c
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-04-04 11:44:30 +0000

    Nusantara-Ify

--- nad_rolex.mk
+++ nad_rolex.mk
@@ -20,15 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Pixel Experience stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+# Inherit some common Nusantara stuff
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
-
-TARGET_OPT_OUT_GCAM_GO := true
-
-# Modify Codename in ZIP
-WAVE_BUILD := rolex-riva
+NAD_BUILD_TYPE := OFFICIAL
+USE_PIXEL_CHARGING := true
 
 # Inherit from rolex device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -37,7 +33,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := nad_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
