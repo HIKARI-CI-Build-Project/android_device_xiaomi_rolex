3647f92cf7e36413289358132206268abfd7d2ac
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-04-19 12:40:37 +0000

    Ancient-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/wave_rolex.mk
+    $(LOCAL_DIR)/ancient_rolex.mk
 
 COMMON_LUNCH_CHOICES := \
-    wave_rolex-user \
-    wave_rolex-userdebug \
-    wave_rolex-eng
+    ancient_rolex-user \
+    ancient_rolex-userdebug \
+    ancient_rolex-eng
