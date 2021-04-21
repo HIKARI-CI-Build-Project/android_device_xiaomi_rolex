99f3cd095fdee221284781c5c094287964c66ef8
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-04-21 13:36:29 +0000

    rova: update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 rm -rf hardware/qcom-caf/msm8996/display 
-git clone https://github.com/Thagoo/hardware_qcom-caf_msm8996-r_display -b eleven hardware/qcom-caf/msm8996/display 
+git clone https://github.com/crdroidandroid/android_hardware_qcom_display -b 11.0-caf-msm8996 hardware/qcom-caf/msm8996/display 
 rm -rf hardware/qcom-caf/msm8996/media
-git clone https://github.com/HANA-CI-Build-Project/android_hardware_qcom_media.git -b 11-89xx hardware/qcom-caf/msm8996/media 
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/media 
 rm -rf hardware/qcom-caf/msm8996/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
